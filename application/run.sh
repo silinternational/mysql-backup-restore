6829414ead08e06e81adf581f5ec51bf1ef53818
Phillip Shipley <phillip.shipley@gmail.com>
2017-08-24 11:14:01 -0400

    Add mysql_host to restore command to be explicit

--- application/run.sh
+++ application/run.sh
@@ -18,7 +18,7 @@ for dbName in ${DB_NAMES}; do
         runny gunzip -f /tmp/${dbName}.sql.gz
 
         start=$(date +%s)
-        runny $(mysql -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" ${dbName} < /tmp/${dbName}.sql)
+        runny $(mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" ${dbName} < /tmp/${dbName}.sql)
         end=$(date +%s)
         logger -p user.info "${dbName} restored in $(expr ${end} - ${start}) seconds."
     else
