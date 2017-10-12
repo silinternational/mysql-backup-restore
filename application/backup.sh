5283cff1a90d41f6b397a948533f3df4d086c22e
Phillip Shipley <phillip.shipley@gmail.com>
2017-10-12 19:47:18 -0400

    attempting to add support for pgsql as well

--- application/backup.sh
+++ application/backup.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env sh
 
+if [[ "x$DB_ENGINE" == "x" ]]; then
+  DB_ENGINE="MYSQL"
+fi
+
+if [[ "$DB_ENGINE" == "MYSQL" ]]; then
+  DUMP_CMD='mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" ${dbName}'
+elif [[ "$DB_ENGINE" == "PGSQL" ]]; then
+  DUMP_CMD='pg_dump -U ${DB_USER} -h ${DB_HOST} ${DB_NAME}'
+fi
+
 for dbName in ${DB_NAMES}; do
     logger -p user.info "backing up ${dbName}..."
 
     start=$(date +%s)
-    runny $(mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" ${dbName} > /tmp/${dbName}.sql)
+    runny $(${DUMP_CMD} > /tmp/${dbName}.sql)
     end=$(date +%s)
     logger -p user.info "${dbName} backed up ($(stat -c %s /tmp/${dbName}.sql) bytes) in $(expr ${end} - ${start}) seconds."
 
