9c0c3ec6f5412a2d14ed66185fe679d104ecda75
Praveen <praveen_koilraj@sil.org>
2025-01-17 11:11:58 +0530

    Minor changes about the indentations

--- application/backup.sh
+++ application/backup.sh
@@ -112,11 +112,11 @@ for dbName in ${DB_NAMES}; do
     if [ "${B2_BUCKET}" != "" ]; then
         start=$(date +%s)
         s3cmd \
-            --access_key=${B2_APPLICATION_KEY_ID} \
-            --secret_key=${B2_APPLICATION_KEY} \
-            --host=${B2_HOST} \
-            --host-bucket='%(bucket)s.'"${B2_HOST}" \
-            put /tmp/${dbName}.sql.gz s3://${B2_BUCKET}/${dbName}.sql.gz
+        --access_key=${B2_APPLICATION_KEY_ID} \
+        --secret_key=${B2_APPLICATION_KEY} \
+        --host=${B2_HOST} \
+        --host-bucket='%(bucket)s.'"${B2_HOST}" \
+        put /tmp/${dbName}.sql.gz s3://${B2_BUCKET}/${dbName}.sql.gz
         STATUS=$?
         end=$(date +%s)
         if [ $STATUS -ne 0 ]; then
@@ -129,5 +129,7 @@ for dbName in ${DB_NAMES}; do
         fi
     fi
 done
+
+echo "mysql-backup-restore: backup: Completed"
 
 exit $STATUS
